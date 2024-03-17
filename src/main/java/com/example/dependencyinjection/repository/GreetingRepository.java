06b0ee61ec2f6ca2775a9d09f2c37140c9286425
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-03-17 15:24:04 +0100

    Repozytorium

---
+++ src/main/java/com/example/dependencyinjection/repository/GreetingRepository.java
@@ -0,0 +1,8 @@
+package com.example.dependencyinjection.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GreetingRepository {
+public String sayGreeting();
+}
